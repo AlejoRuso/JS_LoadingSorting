0ae48670a1d2c1b36ea7b9ba6acc0be9d81f250a
Alejo <istominalexey@ro.ru>
2025-09-19 22:59:37 +0300

    webpack fixed

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/'
+    publicPath: '/JS_LoadingSorting/'
   },
   module: {
     rules: [
